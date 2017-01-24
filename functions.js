f061bc096dc1d6ef8cae9d5dfc7edbef496b158c
Nel Korajkic <nelkorajkic@gmail.com>
2017-01-23 22:31:59 -0500

    added some of the simulation functions, still doesn't work yet, displaying the number of total people and number of simulations made

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 var btn = document.getElementById('btn');
-
+var results = document.getElementById('results');
 function shakeL () {
   btn.style.transform="rotate(-2deg)";
   var looper3 =setTimeout(shakeR,100);
@@ -15,3 +15,55 @@ function btnShake () {
       var looper2 =setTimeout(shakeL,0);
 }
 var looper1 =setInterval(btnShake,2500);
+
+function btnText() {
+  setTimeout(btn.innerHTML='Generate Again',100);
+
+}
+function probability () {
+  var numOfGroups = Math.floor(Math.random() * (3 - 1) + 1);
+  //console.log('num of groups = ' +numOfGroups);
+
+  var groupArray = new Array(numOfGroups);
+  //console.log('length of the array of groups = '+groupArray.length);
+
+  for (i = 0; i < groupArray.length; i++) {
+    groupArray[i] = Math.floor(Math.random() * (10 - 1) + 1);
+}
+birthdayCheck(groupArray);
+}
+function birthdayCheck(num) {
+  //console.log(num);
+  var total = 0;
+  //loops per each group
+  for (var i = 0; i < num.length; i++) {
+    console.log('iteration i number=' + i);
+    var personArray = new Array(num[i]);
+    //loops per each person
+
+    for (var j = 0; j < num[i]; j++) {
+      console.log('iteration j number=' + j);
+      personArray[j] = Math.floor(Math.random() * (10 - 1) + 1);
+      console.log(personArray);
+      // for (var k = 0; k <personArray.length; k++){
+      //   console.log("k:"+personArray[k]);
+      //   console.log("j:"+personArray[j]);
+      //   if (personArray[j] == personArray[k]) {
+      //     console.log("match");
+      //     }
+      //   }
+      }
+
+    total += num[i];
+  }
+
+
+  results.innerHTML = "You generated " + total + " total people  split in "+ num.length +" groups";
+}
+$('#btn').click(function(){
+  btn.innerHTML='Done!';
+  setTimeout(btnText,3000);
+//  $('.container2').show(1000);
+  $('.container2').slideDown(300);
+  probability();
+});
